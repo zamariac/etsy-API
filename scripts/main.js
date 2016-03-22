bf65f71ffaf545dc64465188462f1dea17237ba0
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-22 13:42:16 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+var settings = {
+
+	url: 'https:api.etsy.com/v2/listings/active?api_key={qd4p1ispdj60aj3wybzgpcgb}&keywords={search term}&includes=Images,Shop`',
+	type: 'GET',
+	success: ,
+
+	error: function(){
+
+	}
+	complete: function(){
+
+	}
+};
+
+$.ajax(settings);
+
+
+
+
+//APP NAME zamariac KEYSTRING qd4p1ispdj60aj3wybzgpcgb SHARED SECRET dsz75l03qw
